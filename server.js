68d4e12bee1cf1cddb19eb0be5b3812e05475c97
Coel Tyrell <coel316@yahoo.com>
2019-11-05 12:01:54 -0800

    added location to test

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/cool', (request, response) => {
   response.send('cool data from the /cool route');
 })
 
+app.get('/location', (request, response) => {
+  const geoData = require('./data/geo.json')
+  const city = request.query.data;
+  response.send(city);
+})
+
 app.use( '/', express.static('app') );
 
 app.listen(PORT, () => {
