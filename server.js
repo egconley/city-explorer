b4be058b47cccc2b7d45f2910d0351d9aec688ea
egconley <31393715+egconley@users.noreply.github.com>
2019-11-05 10:47:30 -0800

    tinkered with server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const express = require('express');
 // const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.get('/cool', (request, response) => {
   response.send('cool data from the /cool route');
 })
 
-app.use( express.static('./city-explorer') );
+app.use( '/', express.static('public/index.html') );
 
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
